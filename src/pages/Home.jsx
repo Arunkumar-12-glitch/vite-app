8889a4912e63eaa031fdcc5b5ffa32a273eec535
Arunkumar-12-glitch <arunkumar227264@gmail.com>
2025-03-05 10:25:55 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Arun kumar</h2>
+            <p>Contact: Arun@example.com</p>
+        </div>
+    );
+};
+
+export default Home;
